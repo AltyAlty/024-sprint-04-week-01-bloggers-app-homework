e0ef1dab16ffb9f657da9520143a25b9d41a65dd
AltyAlty <workcsv@gmail.com>
2026-08-10 19:31:05 +0300

    add AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { AppService } from './app.service';
 
 /*Необязательный контроллер для модуля "AppModule".*/
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
   public hello(): string {
-    return 'Hello World!';
+    return this.appService.hello();
   }
 }
